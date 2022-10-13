eadd8c8ff8b67931fbb08875280c7a4a0989a3cc
Dimawesome <d.kolodynskij@gmail.com>
2022-10-13 16:29:14 +0300

    Blades

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('home.index');
+//})->name('home.index');
+
+//Route::get('/contact', function () {
+//    return view('home.contact');
+//})->name('home.contact');
+
+Route::view('/', 'home.index')->name('home.index');
+Route::view('/contact', 'home.contact')->name('home.contact');
+
+Route::get('/articles/{id}', function ($articleId) {
+    $articles = [
+        1 => [
+            'title' => 'First article',
+            'content' => 'First article text 123123',
+            'is_new' => true,
+            'has_comments' => true,
+            'authors' => [
+                1 => [
+                    'name' => 'John',
+                    'surname' => 'Doe'
+                ],
+                2 => [
+                    'name' => 'Vardenis',
+                    'surname' => 'Pavardenis'
+                ],
+                3 => [
+                    'name' => 'Petras',
+                    'surname' => 'Petraitis'
+                ]
+            ],
+        ],
+        2 => [
+            'title' => 'Second article',
+            'content' => 'Second article text 123123',
+            'is_new' => false
+        ],
+    ];
+
+    abort_if(!isset($articles[$articleId]), 404);
+
+    return view('articles.show', ['article' => $articles[$articleId]]);
+})->name('articles.show');
+
+Route::get('/articles/recent/{days?}', function ($days = 25) {
+    return "Articles from $days days ago";
+})->name('articles.recent');
+
