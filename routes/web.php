2c35c0c7fbe8fc2c93a1640ca71d61d05bc0d30f
Dimawesome <d.kolodynskij@gmail.com>
2022-11-30 09:54:58 +0200

    Moved JS, CSS codes to files. Added migration.

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Http\Request;
 use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Auth\LoginController;
-use Illuminate\Support\Facades\App;
-use App\Http\Controllers\LangController;
 
 Route::resource('articles', ArticleController::class);
 
@@ -18,7 +16,3 @@ Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact')
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login.form');
 Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('lang/change', [LangController::class, 'change'])->name('changeLang');
